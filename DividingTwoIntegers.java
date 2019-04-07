10303cad66337cc776a67c929a0cc4ace14b2a91
Srinath <31591297+Srinath-V-S@users.noreply.github.com>
2019-04-07 19:09:15 +0530

    Medium Problems

---
+++ DividingTwoIntegers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+    
+    if(divisor == -1 && dividend == Integer.MIN_VALUE){
+        return Integer.MAX_VALUE;
+    } 
+    
+     int quotient = dividend/divisor;
+        System.out.println(quotient);
+        
+        // if(quotient == Integer.MIN_VALUE){
+        //     return Integer.MAX_VALUE;
+        // }
+        
+        
+    if(quotient < Integer.MIN_VALUE)
+        return Integer.MAX_VALUE;
+    if(quotient > Integer.MAX_VALUE)
+        return Integer.MIN_VALUE;
+            return quotient;
+
+    }
+    
+}
